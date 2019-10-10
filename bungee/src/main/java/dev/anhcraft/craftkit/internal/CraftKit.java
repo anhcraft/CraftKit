5b761fed8e1b0bee1308fdaf40b1cc0eabae3b72
anhcraft <admin@anhcraft.dev>
2019-10-10 22:11:11 +0700

    Removed config file

--- bungee/src/main/java/dev/anhcraft/craftkit/internal/CraftKit.java
+++ bungee/src/main/java/dev/anhcraft/craftkit/internal/CraftKit.java
@@ -1,18 +1,13 @@
 package dev.anhcraft.craftkit.internal;
 
-import dev.anhcraft.craftkit.common.internal.CKConfig;
+import dev.anhcraft.craftkit.chat.ActionBar;
+import dev.anhcraft.craftkit.chat.Chat;
 import dev.anhcraft.craftkit.common.internal.CKInfo;
 import dev.anhcraft.craftkit.common.internal.CKPlugin;
 import dev.anhcraft.craftkit.common.internal.CKProvider;
-import dev.anhcraft.craftkit.helpers.ConfigHelper;
 import dev.anhcraft.craftkit.helpers.TaskHelper;
 import dev.anhcraft.craftkit.internal.listeners.CKChannelListener;
-import dev.anhcraft.craftkit.chat.ActionBar;
-import dev.anhcraft.craftkit.chat.Chat;
-import dev.anhcraft.jvmkit.utils.FileUtil;
 import net.md_5.bungee.api.plugin.Plugin;
-
-import java.io.IOException;
 
 public final class CraftKit extends Plugin implements CKPlugin {
     public static final Chat DEFAULT_CHAT = new Chat("&6#craftkit:&f ");
@@ -31,31 +26,6 @@ public final class CraftKit extends Plugin implements CKPlugin {
 
         // load info
         CKInfo.init(getClass().getResourceAsStream("/ck_info.json"));
-
-        // init files and load the configuration
-        ROOT_DIR.mkdirs();
-        INFO_CHAT.messageConsole("Loading configuration files...");
-        FileUtil.init(CONFIG_FILE, getClass().getResourceAsStream("/config.yml"));
-        new ConfigHelper(CONFIG_FILE).loadTo(CONFIG);
-
-        // check config version
-        if(CKConfig.CONFIG_VERSION < CKInfo.getConfigVersion()){
-            try {
-                WARN_CHAT.messageConsole("Your configuration is outdated! ("+CKConfig.CONFIG_VERSION+")");
-                INFO_CHAT.messageConsole("Attempting to upgrade the configuration...")
-                        .messageConsole("Creating a backup version of the configuration....");
-                OLD_CONFIG_FILE.createNewFile();
-                FileUtil.copy(CONFIG_FILE, OLD_CONFIG_FILE);
-                INFO_CHAT.messageConsole("Creating the new configuration file...");
-                FileUtil.write(CONFIG_FILE, getClass().getResourceAsStream("/config.yml"));
-                INFO_CHAT.messageConsole("Loading the new configuration...");
-                new ConfigHelper(CONFIG_FILE).loadTo(CONFIG);
-                DEFAULT_CHAT.messageConsole("Your configuration file has been updated successfully!")
-                        .messageConsole("The old one is named `"+OLD_CONFIG_FILE.getName() +"` which you can check it later and transfer old values to the new configuration");
-            } catch(IOException e) {
-                e.printStackTrace();
-            }
-        }
 
         // check updates
         checkUpdate(CKPlugin.BUNGEE_RESOURCE_ID);
--- common/src/main/java/dev/anhcraft/craftkit/common/internal/CKPlugin.java
+++ common/src/main/java/dev/anhcraft/craftkit/common/internal/CKPlugin.java
@@ -3,16 +3,9 @@ package dev.anhcraft.craftkit.common.internal;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import dev.anhcraft.craftkit.common.utils.SpigotApiUtil;
-import dev.anhcraft.jvmkit.builders.FilePathBuilder;
-
-import java.io.File;
 
 public interface CKPlugin {
     Gson GSON = new GsonBuilder().setPrettyPrinting().disableHtmlEscaping().create();
-    File ROOT_DIR = new File(new FilePathBuilder().dir("plugins", "CraftKit").build());
-    File CONFIG_FILE = new File(ROOT_DIR, "config.yml");
-    File OLD_CONFIG_FILE = new File(ROOT_DIR, "config.old.yml");
-    CKConfig CONFIG = new CKConfig();
     String CHANNEL_NAMESPACE = "craftkit:plugin";
     String PARTY_CHANNEL_NAMESPACE = "craftkit:party";
     int BUNGEE_RESOURCE_ID = 68505;
