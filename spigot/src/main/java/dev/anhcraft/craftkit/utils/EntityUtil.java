2bac78e4a4f49dde148f49345662369c15370107
anhcraft <admin@anhcraft.dev>
2019-10-20 00:22:48 +0700

    Added methods to display fake equipment

--- spigot/src/main/java/dev/anhcraft/craftkit/utils/EntityUtil.java
+++ spigot/src/main/java/dev/anhcraft/craftkit/utils/EntityUtil.java
@@ -5,7 +5,14 @@ import dev.anhcraft.craftkit.cb_common.internal.services.CBEntityService;
 import dev.anhcraft.craftkit.cb_common.internal.services.ServiceProvider;
 import dev.anhcraft.jvmkit.utils.Condition;
 import org.bukkit.entity.Entity;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.EquipmentSlot;
+import org.bukkit.inventory.ItemStack;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.stream.Collectors;
 
 /**
  * Utilities related to entity
@@ -33,5 +40,33 @@ public class EntityUtil {
         Condition.argNotNull("entity", entity);
         Condition.argNotNull("box", box);
         SERVICE.setBoundingBox(entity, box);
+    }
+
+    /**
+     * Displays fake equipment over the body of an entity
+     * @param id the entity's id
+     * @param slot the slot
+     * @param itemStack stack of items
+     * @param viewer who can view the fake equipment
+     */
+    public static void displayEquipment(int id, @NotNull EquipmentSlot slot, ItemStack itemStack, @NotNull Player viewer){
+        Condition.argNotNull("slot", slot);
+        Condition.argNotNull("itemStack", itemStack);
+        Condition.argNotNull("viewer", viewer);
+        SERVICE.displayItem(id, slot, itemStack, Collections.singletonList(SERVICE.toNms(viewer)));
+    }
+
+    /**
+     * Displays fake equipment over the body of an entity
+     * @param id the entity's id
+     * @param slot the slot
+     * @param itemStack stack of items
+     * @param viewers who can view the fake equipment
+     */
+    public static void displayEquipment(int id, @NotNull EquipmentSlot slot, ItemStack itemStack, @NotNull Collection<Player> viewers){
+        Condition.argNotNull("slot", slot);
+        Condition.argNotNull("itemStack", itemStack);
+        Condition.argNotNull("viewers", viewers);
+        SERVICE.displayItem(id, slot, itemStack, viewers.stream().map(SERVICE::toNms).collect(Collectors.toList()));
     }
 }
