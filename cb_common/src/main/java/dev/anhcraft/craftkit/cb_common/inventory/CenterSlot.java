898408f49e6e2bb709ce13fe87d5f1152ac5b0ac
anhcraft <admin@anhcraft.dev>
2019-10-20 08:51:31 +0700

    Added CenterSlot

---
+++ cb_common/src/main/java/dev/anhcraft/craftkit/cb_common/inventory/CenterSlot.java
@@ -0,0 +1,94 @@
+package dev.anhcraft.craftkit.cb_common.inventory;
+
+/**
+ * Contains center slot.
+ */
+public class CenterSlot {
+    /**
+     * The single center position.
+     */
+    public static final int CENTER_POS = 4;
+
+    /**
+     * The center position of first stuff in two ones.
+     */
+    public static final int CENTER_POS_2_A = 2;
+    /**
+     * The center position of second stuff in two ones.
+     */
+    public static final int CENTER_POS_2_B = 6;
+
+    /**
+     * The center position of first stuff in three ones.
+     */
+    public static final int CENTER_POS_3_A = 1;
+    /**
+     * The center position of second stuff in three ones.
+     */
+    public static final int CENTER_POS_3_B = 4;
+    /**
+     * The center position of third stuff in three ones.
+     */
+    public static final int CENTER_POS_3_C = 7;
+
+    /**
+     * The center position of first stuff in four ones.
+     */
+    public static final int CENTER_POS_4_A = 1;
+    /**
+     * The center position of second stuff in four ones.
+     */
+    public static final int CENTER_POS_4_B = 3;
+    /**
+     * The center position of three stuff in four ones.
+     */
+    public static final int CENTER_POS_4_C = 5;
+    /**
+     * The center position of forth stuff in four ones.
+     */
+    public static final int CENTER_POS_4_D = 7;
+
+    /**
+     * The center position of first stuff in five ones.
+     */
+    public static final int CENTER_POS_5_A = 0;
+    /**
+     * The center position of second stuff in five ones.
+     */
+    public static final int CENTER_POS_5_B = 2;
+    /**
+     * The center position of third stuff in five ones.
+     */
+    public static final int CENTER_POS_5_C = 4;
+    /**
+     * The center position of forth stuff in five ones.
+     */
+    public static final int CENTER_POS_5_D = 6;
+    /**
+     * The center position of fifth stuff in five ones.
+     */
+    public static final int CENTER_POS_5_E = 8;
+
+    public static final int[] CENTER_POS_2 = new int[]{
+            CENTER_POS_2_A,
+            CENTER_POS_2_B,
+    };
+    public static final int[] CENTER_POS_3 = new int[]{
+            CENTER_POS_3_A,
+            CENTER_POS_3_B,
+            CENTER_POS_3_C,
+    };
+    public static final int[] CENTER_POS_4 = new int[]{
+            CENTER_POS_4_A,
+            CENTER_POS_4_B,
+            CENTER_POS_4_C,
+            CENTER_POS_4_D,
+    };
+    public static final int[] CENTER_POS_5 = new int[]{
+            CENTER_POS_5_A,
+            CENTER_POS_5_B,
+            CENTER_POS_5_C,
+            CENTER_POS_5_D,
+            CENTER_POS_5_E
+    };
+}
