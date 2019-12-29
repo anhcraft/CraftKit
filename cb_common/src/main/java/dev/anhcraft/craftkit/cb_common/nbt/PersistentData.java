a74077b18f0768a80d316d24f089eda1fd91ca5a
anhcraft <huynhduyanh123123@gmail.com>
2019-12-29 10:28:57 +0700

    Added PersistentData

---
+++ cb_common/src/main/java/dev/anhcraft/craftkit/cb_common/nbt/PersistentData.java
@@ -0,0 +1,7 @@
+package dev.anhcraft.craftkit.cb_common.nbt;
+
+public class PersistentData {
+    public static final String OF_ITEM = "PublicBukkitValues";
+    public static final String OF_BLOCK = "PublicBukkitValues";
+    public static final String OF_ENTITY = "BukkitValues";
+}
