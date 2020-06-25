0411340ed77e7ec90257b7a6f34f5f4ee3e25921
anhcraft <huynhduyanh123123@gmail.com>
2020-06-25 19:21:28 +0700

    removed legacy hand slots

--- spigot/src/main/java/dev/anhcraft/craftkit/HandSlot.java
+++ spigot/src/main/java/dev/anhcraft/craftkit/HandSlot.java
@@ -4,11 +4,6 @@ package dev.anhcraft.craftkit;
  * Types of hand slots.
  */
 public enum HandSlot {
-    @Deprecated
-    MAINHAND,
-    @Deprecated
-    OFFHAND,
-
     /**
      * Main hand slot.
      */
