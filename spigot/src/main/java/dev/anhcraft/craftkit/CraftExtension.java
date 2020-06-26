066d7caec0a43d117daba199a80a7c244ad3844f
anhcraft <huynhduyanh123123@gmail.com>
2020-06-26 19:59:19 +0700

    allowed async call to CraftExtension#unregister

--- spigot/src/main/java/dev/anhcraft/craftkit/CraftExtension.java
+++ spigot/src/main/java/dev/anhcraft/craftkit/CraftExtension.java
@@ -1,10 +1,10 @@
 package dev.anhcraft.craftkit;
 
-import dev.anhcraft.craftkit.cb_common.internal.backend.CBAnvilBackend;
-import dev.anhcraft.craftkit.cb_common.internal.backend.CBCustomGUIBackend;
-import dev.anhcraft.craftkit.cb_common.internal.backend.BackendManager;
 import dev.anhcraft.craftkit.cb_common.gui.AnvilGUI;
 import dev.anhcraft.craftkit.cb_common.gui.CustomGUI;
+import dev.anhcraft.craftkit.cb_common.internal.backend.BackendManager;
+import dev.anhcraft.craftkit.cb_common.internal.backend.CBAnvilBackend;
+import dev.anhcraft.craftkit.cb_common.internal.backend.CBCustomGUIBackend;
 import dev.anhcraft.craftkit.common.ICraftExtension;
 import dev.anhcraft.craftkit.common.internal.CKInfo;
 import dev.anhcraft.craftkit.common.internal.Supervisor;
@@ -15,7 +15,6 @@ import dev.anhcraft.craftkit.helpers.TaskHelper;
 import dev.anhcraft.craftkit.internal.CraftKit;
 import dev.anhcraft.jvmkit.utils.Condition;
 import dev.anhcraft.jvmkit.utils.MathUtil;
-import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.InventoryHolder;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -38,8 +37,9 @@ public class CraftExtension implements ICraftExtension<JavaPlugin> {
      */
     public static void unregister(@NotNull Class<? extends JavaPlugin> mainClass){
         Condition.argNotNull("mainClass", mainClass);
-        Condition.check(Bukkit.isPrimaryThread(), "Async catch! Require calling from main thread!");
-        REGISTRY.remove(mainClass);
+        synchronized (LOCK) {
+            REGISTRY.remove(mainClass);
+        }
     }
 
     /**
